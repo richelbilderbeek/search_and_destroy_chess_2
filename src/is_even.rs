85c6cf6b2b5953b3f0033fa130676c3bb1df4643
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-16 20:17:04 +0100

    Test 'is_even'

--- src/is_even.rs
+++ src/is_even.rs
@@ -1,3 +1,14 @@
 pub fn is_even(value: i32) -> bool {
     value % 2 == 0
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_even_works() {
+        assert_eq!(is_even(1), false);
+        assert_eq!(is_even(2), true);
+    }
+}
