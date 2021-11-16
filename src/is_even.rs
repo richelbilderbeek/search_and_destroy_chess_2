a07ba5ded75655fe917e15be94e2366823ca8dd6
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-16 20:04:27 +0100

    First function

---
+++ src/is_even.rs
@@ -0,0 +1,3 @@
+//fn is_even(value: i32) -> i32 {
+//    value % 2 == 0
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn is_even(value: i32) -> bool {
+    value % 2 == 0
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("2: {}", is_even(2));
+    println!("1: {}", is_even(1));
 }
