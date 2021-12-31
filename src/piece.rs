6e34a5a7deae7c253b17cfe73b9502ea4f1f9ad3
richelbilderbeek <richel@richelbilderbeek.nl>
2021-12-31 12:22:11 +0100

    Can create white pawn

--- src/piece.rs
+++ src/piece.rs
@@ -9,6 +9,23 @@ pub struct Piece {
     r#type: PieceType,
 }
 
+pub fn create_white_pawn(position: &str) -> Piece {
+    Piece{
+        color: Color::White,
+        position: Square::new(position),
+        r#type: PieceType::Pawn
+    }
+}
+
+pub fn create_starting_pieces() -> Vec<Piece> {
+    let pieces: Vec<Piece> = vec![
+        create_white_pawn("a2"),
+        create_white_pawn("b2"),
+    ];
+    pieces
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,6 +40,18 @@ mod tests {
         assert_eq!(piece.color, Color::White);
         assert_eq!(piece.position, Square::new("a2"));
         assert_eq!(piece.r#type, PieceType::Pawn);
+    }
+    fn create_a3_pawn() {
+        let piece = create_white_pawn("b2");
+        assert_eq!(piece.color, Color::White);
+        assert_eq!(piece.position, Square::new("b2"));
+        assert_eq!(piece.r#type, PieceType::Pawn);
+    }
+
+    #[test]
+    fn starting_pieces() {
+        let pieces = create_starting_pieces();
+        assert_ne!(pieces.len(), 0);
     }
 
 }
