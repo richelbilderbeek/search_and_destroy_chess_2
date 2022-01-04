a7193d8040049796df9a77f6be6646117666d9bb
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-04 16:51:16 +0100

    Put class members as such

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,42 @@ use sfml::{
 use std::{env, f32::consts::PI};
 
 struct GameView {
-
+    game_width: u32,
+    game_height: u32,
+    window: std::cell::RefCell<RenderWindow>,
+    paddle_size: Vector2f,
+    ball_radius: f32,
 }
 
 impl GameView {
     pub fn new() -> GameView {
-        GameView {}
-    }
-    pub fn run(&self) {
-        let mut rng = thread_rng();
 
         // Define some constants
         let game_width = 800;
         let game_height = 600;
-        let paddle_size = Vector2f::new(25., 100.);
-        let ball_radius = 10.;
 
         // Create the window of the application
         let context_settings = ContextSettings::default();
-        let mut window = RenderWindow::new(
-            (game_width, game_height),
-            "SFML Pong",
-            Style::CLOSE,
-            &context_settings,
+        let window = std::cell::RefCell::new(
+            RenderWindow::new(
+                (game_width, game_height),
+                "SFML Pong",
+                Style::CLOSE,
+                &context_settings,
+            )
         );
-        window.set_vertical_sync_enabled(true);
+        let paddle_size = Vector2f::new(25., 100.);
+        GameView {
+            game_width,
+            game_height,
+            window,
+            paddle_size,
+            ball_radius: 10.,
+        }
+    }
+    pub fn run(&self) {
+        let mut rng = thread_rng();
+        self.window.borrow_mut().set_vertical_sync_enabled(true);
 
         // Load the sounds used in the game
         let ball_soundbuffer = SoundBuffer::from_file("assets/examples_resources_ball.wav").unwrap();
@@ -43,27 +54,27 @@ impl GameView {
 
         // Create the left paddle
         let mut left_paddle = RectangleShape::new();
-        left_paddle.set_size(paddle_size - 3.);
+        left_paddle.set_size(self.paddle_size - 3.);
         left_paddle.set_outline_thickness(3.);
         left_paddle.set_outline_color(Color::BLACK);
         left_paddle.set_fill_color(Color::rgb(100, 100, 200));
-        left_paddle.set_origin(paddle_size / 2.);
+        left_paddle.set_origin(self.paddle_size / 2.);
 
         // Create the right paddle
         let mut right_paddle = RectangleShape::new();
-        right_paddle.set_size(paddle_size - 3.);
+        right_paddle.set_size(self.paddle_size - 3.);
         right_paddle.set_outline_thickness(3.);
         right_paddle.set_outline_color(Color::BLACK);
         right_paddle.set_fill_color(Color::rgb(200, 100, 100));
-        right_paddle.set_origin(paddle_size / 2.);
+        right_paddle.set_origin(self.paddle_size / 2.);
 
         // Create the ball
         let mut ball = CircleShape::default();
-        ball.set_radius(ball_radius - 3.);
+        ball.set_radius(self.ball_radius - 3.);
         ball.set_outline_thickness(3.);
         ball.set_outline_color(Color::BLACK);
         ball.set_fill_color(Color::WHITE);
-        ball.set_origin((ball_radius / 2., ball_radius / 2.));
+        ball.set_origin((self.ball_radius / 2., self.ball_radius / 2.));
 
         // Load the text font
         let font = Font::from_file("assets/sansation.ttf").unwrap();
@@ -90,7 +101,7 @@ impl GameView {
         let mut down = false;
 
         loop {
-            while let Some(event) = window.poll_event() {
+            while let Some(event) = &self.window.borrow_mut().poll_event() {
                 match event {
                     Event::Closed
                     | Event::KeyPressed {
@@ -105,12 +116,12 @@ impl GameView {
                         ball_sound.set_pitch(1.0);
                         clock.restart();
                         // Reset the position of the paddles and ball
-                        left_paddle.set_position((10. + paddle_size.x / 2., game_height as f32 / 2.));
+                        left_paddle.set_position((10. + self.paddle_size.x / 2., self.game_height as f32 / 2.));
                         right_paddle.set_position((
-                            game_width as f32 - 10. - paddle_size.x / 2.,
-                            game_height as f32 / 2.,
+                            self.game_width as f32 - 10. - self.paddle_size.x / 2.,
+                            self.game_height as f32 / 2.,
                         ));
-                        ball.set_position((game_width as f32 / 2., game_height as f32 / 2.));
+                        ball.set_position((self.game_width as f32 / 2., self.game_height as f32 / 2.));
                         // Reset the ball angle
                         loop {
                             // Make sure the ball initial angle is not too much vertical
@@ -136,17 +147,17 @@ impl GameView {
                 let delta_time = clock.restart().as_seconds();
 
                 // Move the player's paddle
-                if up && (left_paddle.position().y - paddle_size.y / 2. > 5.) {
+                if up && (left_paddle.position().y - self.paddle_size.y / 2. > 5.) {
                     left_paddle.move_((0., -paddle_speed * delta_time));
                 }
-                if down && (left_paddle.position().y + paddle_size.y / 2. < game_height as f32 - 5.) {
+                if down && (left_paddle.position().y + self.paddle_size.y / 2. < self.game_height as f32 - 5.) {
                     left_paddle.move_((0., paddle_speed * delta_time));
                 }
 
                 // Move the computer's paddle
-                if ((right_paddle_speed < 0.) && (right_paddle.position().y - paddle_size.y / 2. > 5.))
+                if ((right_paddle_speed < 0.) && (right_paddle.position().y - self.paddle_size.y / 2. > 5.))
                     || ((right_paddle_speed > 0.)
-                        && (right_paddle.position().y + paddle_size.y / 2. < game_height as f32 - 5.))
+                        && (right_paddle.position().y + self.paddle_size.y / 2. < self.game_height as f32 - 5.))
                 {
                     right_paddle.move_((0., right_paddle_speed * delta_time));
                 }
@@ -154,11 +165,11 @@ impl GameView {
                 // Update the computer's paddle direction according to the ball position
                 if ai_timer.elapsed_time() > ai_time {
                     ai_timer.restart();
-                    if ball.position().y + ball_radius > right_paddle.position().y + paddle_size.y / 2.
+                    if ball.position().y + self.ball_radius > right_paddle.position().y + self.paddle_size.y / 2.
                     {
                         right_paddle_speed = paddle_speed;
-                    } else if ball.position().y - ball_radius
-                        < right_paddle.position().y - paddle_size.y / 2.
+                    } else if ball.position().y - self.ball_radius
+                        < right_paddle.position().y - self.paddle_size.y / 2.
                     {
                         right_paddle_speed = -paddle_speed;
                     } else {
@@ -171,33 +182,33 @@ impl GameView {
                 ball.move_((ball_angle.cos() * factor, ball_angle.sin() * factor));
 
                 // Check collisions between the ball and the screen
-                if ball.position().x - ball_radius < 0. {
+                if ball.position().x - self.ball_radius < 0. {
                     is_playing = false;
                     pause_message.set_string("You lost !\nPress space to restart or\nescape to exit");
                 }
-                if ball.position().x + ball_radius > game_width as f32 {
+                if ball.position().x + self.ball_radius > self.game_width as f32 {
                     is_playing = false;
                     pause_message.set_string("You won !\nPress space to restart or\nescape to exit");
                 }
-                if ball.position().y - ball_radius < 0. {
+                if ball.position().y - self.ball_radius < 0. {
                     on_bounce(&mut ball_sound, &mut ball_speed);
                     ball_angle = -ball_angle;
                     let p = ball.position().x;
-                    ball.set_position((p, ball_radius + 0.1));
+                    ball.set_position((p, self.ball_radius + 0.1));
                 }
-                if ball.position().y + ball_radius > game_height as f32 {
+                if ball.position().y + self.ball_radius > self.game_height as f32 {
                     on_bounce(&mut ball_sound, &mut ball_speed);
                     ball_angle = -ball_angle;
                     let p = ball.position().x;
-                    ball.set_position((p, game_height as f32 - ball_radius - 0.1));
+                    ball.set_position((p, self.game_height as f32 - self.ball_radius - 0.1));
                 }
 
                 // Check the collisions between the ball and the paddles
                 // Left Paddle
                 let (ball_pos, paddle_pos) = (ball.position(), left_paddle.position());
-                if ball_pos.x - ball_radius < paddle_pos.x + paddle_size.x / 2.
-                    && ball_pos.y + ball_radius >= paddle_pos.y - paddle_size.y / 2.
-                    && ball_pos.y - ball_radius <= paddle_pos.y + paddle_size.y / 2.
+                if ball_pos.x - self.ball_radius < paddle_pos.x + self.paddle_size.x / 2.
+                    && ball_pos.y + self.ball_radius >= paddle_pos.y - self.paddle_size.y / 2.
+                    && ball_pos.y - self.ball_radius <= paddle_pos.y + self.paddle_size.y / 2.
                 {
                     if ball_pos.y > paddle_pos.y {
                         ball_angle = PI - ball_angle + rng.gen_range(0.0..20.) * PI / 180.;
@@ -207,16 +218,16 @@ impl GameView {
 
                     on_bounce(&mut ball_sound, &mut ball_speed);
                     ball.set_position((
-                        paddle_pos.x + ball_radius + paddle_size.x / 2. + 0.1,
+                        paddle_pos.x + self.ball_radius + self.paddle_size.x / 2. + 0.1,
                         ball_pos.y,
                     ));
                 }
 
                 // Right Paddle
                 let (ball_pos, paddle_pos) = (ball.position(), right_paddle.position());
-                if ball_pos.x + ball_radius > paddle_pos.x - paddle_size.x / 2.
-                    && ball_pos.y + ball_radius >= paddle_pos.y - paddle_size.y / 2.
-                    && ball_pos.y - ball_radius <= paddle_pos.y + paddle_size.y / 2.
+                if ball_pos.x + self.ball_radius > paddle_pos.x - self.paddle_size.x / 2.
+                    && ball_pos.y + self.ball_radius >= paddle_pos.y - self.paddle_size.y / 2.
+                    && ball_pos.y - self.ball_radius <= paddle_pos.y + self.paddle_size.y / 2.
                 {
                     if ball_pos.y > paddle_pos.y {
                         ball_angle = PI - ball_angle + rng.gen_range(0.0..20.) * PI / 180.;
@@ -226,26 +237,26 @@ impl GameView {
 
                     on_bounce(&mut ball_sound, &mut ball_speed);
                     ball.set_position((
-                        paddle_pos.x - ball_radius - paddle_size.x / 2. - 0.1,
+                        paddle_pos.x - self.ball_radius - self.paddle_size.x / 2. - 0.1,
                         ball_pos.y,
                     ));
                 }
             }
             // Clear the window
-            window.clear(Color::rgb(50, 200, 50));
+            self.window.borrow_mut().clear(Color::rgb(50, 200, 50));
 
             if is_playing {
                 // Draw the paddles and the ball
-                window.draw(&left_paddle);
-                window.draw(&right_paddle);
-                window.draw(&ball);
+                self.window.borrow_mut().draw(&left_paddle);
+                self.window.borrow_mut().draw(&right_paddle);
+                self.window.borrow_mut().draw(&ball);
             } else {
                 // Draw the pause message
-                window.draw(&pause_message);
+                self.window.borrow_mut().draw(&pause_message);
             }
 
             // Display things on screen
-            window.display()
+            self.window.borrow_mut().display()
         }
     }
 }
